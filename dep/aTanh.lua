0b150f533ee205fe8c6c8e1aa70733f7da212eae
ano <anoidgit@users.noreply.github.com>
2016-11-29 13:00:48 +0800

    Create aTanh.lua

---
+++ dep/aTanh.lua
@@ -0,0 +1,37 @@
+--[[
+	The GNU GENERAL PUBLIC LICENSE Version 3
+
+	Copyright (c) 2016 Hongfei Xu
+
+	This scripts implement a standard tanh activation function
+
+	Version 0.0.1
+
+]]
+
+local aTanh = torch.class('nn.aTanh', 'nn.Module')
+
+function aTanh:__init()
+
+	self.backward = self.updateGradInput
+
+end
+
+function aTanh:updateOutput(input)
+
+	self.output = input:tanh()
+
+	return self.output
+
+end
+
+function aTanh:updateGradInput(input, gradOutput)
+
+	local gradInput = input.new()
+	gradInput:resizeAs(input):fill(1)
+	gradInput:addcmul(-1,gradOutput,gradOutput)
+	self.gradInput = gradInput
+
+	return gradInput
+
+end
