3355e9bc8e04b72ef4649c8a3131b864d9151ffc
ano <anoidgit@users.noreply.github.com>
2016-12-03 10:49:00 +0800

    Update aSeqTanh.lua
    fix memory leaky bugs

--- dep/aSeqTanh.lua
+++ dep/aSeqTanh.lua
@@ -3,19 +3,19 @@
 
 	Copyright (c) 2016 Hongfei Xu
 
-	This scripts implement a standard tanh activation function,
+	This scripts implement a standard sigmoid activation function,
 	It forward the sequence from head to tail,
 	and backward in reverse order.
 
 	Designed for Recurrent Neural Networks
 
-	Version 0.0.5
+	Version 0.0.4
 
 ]]
 
-local aSeqTanh = torch.class('nn.aSeqTanh', 'nn.Module')
+local aSeqSigmoid = torch.class('nn.aSeqSigmoid', 'nn.Module')
 
-function aSeqTanh:__init(reverseOrder)
+function aSeqSigmoid:__init(reverseOrder)
 
 	if reverseOrder then
 		self.rindex = nil
@@ -27,29 +27,50 @@ function aSeqTanh:__init(reverseOrder)
 
 end
 
-function aSeqTanh:backward(input, gradOutput, scale)
+-- evaluate
+function aSeqSigmoid:evaluate()
+
+	self.train = false
+
+	self:forget()
+
+end
+
+-- train
+function aSeqSigmoid:training()
+
+	self.train = true
+
+	self:forget()
+
+end
+
+function aSeqSigmoid:backward(input, gradOutput, scale)
 
 	return self:updateGradInput(input, gradOutput)
 
 end
 
-function aSeqTanh:updateOutput(input)
+function aSeqSigmoid:updateOutput(input)
 
-	local output = torch.tanh(input)
-	table.insert(self._output,output)
+	local output = torch.sigmoid(input)
+	if self.train then
+		table.insert(self._output,output)
+	end
 	self.output = output
 
 	return self.output
 
 end
 
-function aSeqTanh:updateGradInput(input, gradOutput)
+function aSeqSigmoid:updateGradInput(input, gradOutput)
 
 	local output = table.remove(self._output, self.rindex)
 
 	local gradInput = input.new()
 	gradInput:resizeAs(input):fill(1)
-	gradInput:addcmul(-1, output, output)
+	gradInput:csub(output)
+	gradInput:cmul(output)
 	gradInput:cmul(gradOutput)
 	self.gradInput = gradInput
 
@@ -59,7 +80,7 @@ end
 
 -- Warning: This method is dangerous,
 -- unless you know what you are doing.
-function aSeqTanh:forget()
+function aSeqSigmoid:forget()
 
 	self._output = {}
 
