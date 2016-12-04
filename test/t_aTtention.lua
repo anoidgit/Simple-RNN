1862ad64f65caf51f96bcac9a42d8859ccffca65
ano <anoidgit@users.noreply.github.com>
2016-12-04 12:35:20 +0800

    Create t_aTtention.lua

---
+++ test/t_aTtention.lua
@@ -0,0 +1,26 @@
+print("set default tensor type")
+torch.setdefaulttensortype('torch.FloatTensor')
+
+seqlen = 2
+batchsize = 4
+vecsize = 4
+
+require "nn"
+require "aSeqBiLinear"
+require "aTtention"
+
+nn.aLinear = nn.Linear
+nn.aSequential = nn.Sequential
+nn.aSoftMax = nn.SoftMax
+
+tmod = nn.aTtention(vecsize, true)
+tmod:training()
+td1 = torch.randn(seqlen,batchsize,vecsize)
+td2 = torch.randn(batchsize,vecsize)
+ti = {td1, td2}
+rs = tmod:forward(ti)
+print(rs)
+grad = tmod:backward(ti, torch.randn(batchsize*vecsize))
+print(grad)
+print(grad[1])
+print(grad[2])
