75301614870c743f0e5f6727bd0ac963bfe52435
ano <anoidgit@users.noreply.github.com>
2016-12-04 11:08:53 +0800

    Create t_aSeqBiLinear.lua

---
+++ test/t_aSeqBiLinear.lua
@@ -0,0 +1,23 @@
+print("set default tensor type")
+torch.setdefaulttensortype('torch.FloatTensor')
+
+seqlen = 2
+batchsize = 4
+vecsize = 4
+
+require "nn"
+require "aSeqBiLinear"
+
+nn.aLinear = nn.Linear
+
+tmod = nn.aSeqBiLinear(vecsize, true)
+tmod:training()
+td1 = torch.randn(seqlen,batchsize,vecsize)
+td2 = torch.randn(batchsize,vecsize)
+ti = {td1, td2}
+rs = tmod:forward(ti)
+print(rs)
+grad = tmod:backward(ti, torch.randn(seqlen*batchsize))
+print(grad)
+print(grad[1])
+print(grad[2])
