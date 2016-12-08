56ef9d81ec82235c228f0508904647cc3730db10
ano <anoidgit@users.noreply.github.com>
2016-12-08 11:05:30 +0800

    Update aNMT.lua

--- model/aNMT.lua
+++ model/aNMT.lua
@@ -5,21 +5,20 @@
 
 	This scripts implement an NMT for RNN:
 
-	Version 0.0.4
+	Version 0.0.5
 
 ]]
 
 local aNMT, parent = torch.class('nn.aNMT', 'nn.aBstractNMT')
 
 -- generate a module
-function aNMT:__init(encoder, decoder, attention, lookup, classifier, eosid, max_Target_Length, evaOTag)
+function aNMT:__init(encoder, decoder, attention, classifier, eosid, max_Target_Length, evaOTag)
 
 	parent.__init(self)
 
 	self.encoder = encoder-- a Sequence module input the whole sequence
 	self.decoder = decoder-- a Step module process the sequence step by step
 	self.attention = attention
-	self.lookup = lookup
 	self.classifier = classifier
 	self.eosid = eosid
 	self.maxLength = max_Target_Length or 256
@@ -43,7 +42,7 @@ function aNMT:_seq_updateOutput(input)
 	-- prepare the whole sequence
 	local _cOutput = self._encoded[-1]
 	local _cAttention = self.attention:updateOutput({self._encoded, _cOutput})
-	self._initInput = torch.cat(_cOutput, _cAttention)
+	self._initInput = {_cOutput, _cAttention}
 	local _nInput = self._initInput
 
 	local _output = {}-- store the result
@@ -53,7 +52,7 @@ function aNMT:_seq_updateOutput(input)
 		for _ = 1, #tar do
 			_cOutput = self.decoder:updateOutput(_nInput)
 			_cAttention = self.attention:updateOutput({self._encoded, _cOutput})
-			_nInput = torch.cat(_cOutput, _cAttention)
+			_nInput = {_cOutput, _cAttention}
 			table.insert(_output, _nInput)
 		end
 
@@ -63,15 +62,15 @@ function aNMT:_seq_updateOutput(input)
 		while not self.state:all() and _length < self.maxLength do
 			_cOutput = self.decoder:updateOutput(_nInput)
 			_cAttention = self.attention:updateOutput({self._encoded, _cOutput})
-			_nInput = torch.cat(_cOutput, _cAttention)
+			_nInput = {_cOutput, _cAttention}
 			local _rs = self.classifier:updateOutput(_nInput)
 			self:_updateState(_rs)
-			_length = _length + 1
 			if self.evaOTag then
 				table.insert(_output, _rs)
 			else
 				table.insert(_output, _nInput)
 			end
+			_length = _length + 1
 		end
 	end
 
@@ -83,46 +82,54 @@ end
 
 function aNMT:_seq_backward(input, gradOutput, scale)
 
-	local tGradInput
-
 	local src = input[1]
 	local tar = input[2]
 
-	-- backward the decoder
-	local _gEncoder, _gDecoder, __gEncoder
-	local _gInput
+	local _gEncoder
+
+	local _cOutput = table.remove(self.output)[1]
+
+	local _gLOutput, _gLAttention
+
 	for _ = #tar, 1, -1 do
 
-		local _cAttention = table.remove(self.oattention)
-		local _cDecoder = table.remove(self.odecoder)
-		local _cGrad = table.remove(gradOutput)
-		local _cInput = table.remove(tar)
-
-		if _gInput then
-			_cGrad:add(_gInput)
-		end
-
-		if _gEncoder then
-			__gEncoder, _gDecoder = self.attention:backward({self._encoded, _cDecoder}, )
-			_gEncoder:add(__gEncoder)
-		else
-			_gEncoder, _gDecoder = self.attention:backward({self._encoded, _cDecoder}, )
-		end
-
-		_gInput = self.decoder:backward(_cInput, _gDecoder, scale)
+		local _prevOutput
 
 		if _ > 1 then
-			tGradInput[_] = _gInput:clone()
+			_prevOutput, _prevAttention = unpack(table.remove(self.output))
+		else
+			_prevOutput, _prevAttention = unpack(self._initInput)
 		end
+
+		local _cGradOutput, _cGradAttention = unpack(table.remove(gradOutput))
+
+		if _gLOutput then
+			_cGradOutput:add(_gLOutput)
+			_cGradAttention:add(_gLAttention)
+		end
+
+		local __gEncoder, _gOutput = self.attention:backward({self.encoded, _cOutput}, _cGradAttention, scale)
+
+		if _gEncoder then
+			_gEncoder:add(__gEncoder)
+		else
+			_gEncoder = __gEncoder
+		end
+
+		_cGradOutput:add(_gOutput)
+
+		_gLOutput, _gLAttention = unpack(self.decoder:backward({_prevOutput, _prevAttention}, _cGradOutput, scale))
+
+		_cOutput = _prevOutput
 
 	end
 
-	tGradInput[1] = _gInput
+	_gEncoder[-1]:add(_gLOutput)
 
 	self:_copy_backward(decoder, encoder)
 
 	local sGradInput = self.encoder:backward(src, self:_tranTensor(_gEncoder), scale)
 
-	return {sGradInput, tGradInput}
+	return {sGradInput}
 
 end
