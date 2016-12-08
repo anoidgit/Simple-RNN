31aa0f574d52150cf62f4c58b93fd025c94d6f98
ano <anoidgit@users.noreply.github.com>
2016-12-08 13:34:30 +0800

    Create conf.lua

---
+++ experiment/NMT/conf.lua
@@ -0,0 +1,13 @@
+--[[
+	The GNU GENERAL PUBLIC LICENSE Version 3
+
+	Copyright (c) 2016 Hongfei Xu
+
+	This scripts controls which config to use
+
+	Version 0.0.1
+
+]]
+
+--require "mconf"
+require "aconf"
