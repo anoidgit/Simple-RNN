1ba9a42e3eac3227b0b7c3c9ca7b9468c9650415
ano <anoidgit@users.noreply.github.com>
2016-12-08 13:41:21 +0800

    Create nnsettings.lua

---
+++ experiment/NMT/nnsettings.lua
@@ -0,0 +1,15 @@
+--[[
+	The GNU GENERAL PUBLIC LICENSE Version 3
+
+	Copyright (c) 2016 Hongfei Xu
+
+	This scripts defines the NMT model
+
+	Version 0.0.1
+
+]]
+
+hiddenSize = {}
+for _ = 1, 8 do
+  table.insert(hiddenSize, sizvec)
+end
