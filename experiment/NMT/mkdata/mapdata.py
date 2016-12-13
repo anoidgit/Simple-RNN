77b17391d025c1699f2691334e7a9f768bac6cc2
ano <anoidgit@users.noreply.github.com>
2016-12-13 10:27:07 +0800

    Create mapdata.py

---
+++ experiment/NMT/mkdata/mapdata.py
@@ -0,0 +1,62 @@
+#encoding: utf-8
+
+import sys
+reload(sys)
+sys.setdefaultencoding( "utf-8" )
+
+def ldd(df):
+	rsd={}
+	with open(df) as frd:
+		for line in frd:
+			tmp=line.strip()
+			if tmp:
+				tmp=tmp.decode("utf-8")
+				k,v=tmp.split("	")
+				rsd[k]=v
+	return rsd
+
+def mapfile(srcf,rsf,mapd,unkid,addf,addv,padv,reverseOrder):
+	maxlen=0
+	maprs=[]
+	with open(srcf) as frd:
+		for line in frd:
+			tmp=line.strip()
+			if tmp:
+				tmp=tmp.decode("utf-8")
+				rs=[mapd.get(tmpu,unkid) for tmpu in tmp]
+				maprs.append(rs)
+				clen=len(rs)
+				if clen>maxlen:
+					maxlen=clen
+	for mapu in maprs:
+		clen=len(mapu)
+		if addf:
+			mapu.insert(0,addv)
+		else:
+			mapu.append(addv)
+		if clen<maxlen:
+			ext=[padv for i in xrange(maxlen-clen)]
+			mapu.extend(ext)
+		if reverseOrder:
+			mapu.reverse()
+	with open(rsf,"w") as fwrt:
+		tmp="\n".join([" ".join(lind) for lind in maprs])
+		fwrt.write(tmp.encode("utf-8"))
+
+def mapfl(srcfl,rsfl,mapf):
+	isEncoder=False
+	mapd=ldd(mapf)
+	addv=mapd["EOS"]#EOS,SOS
+	unkid=mapd["UNK"]
+	crs=0
+	for srcf in srcfl:
+		mapfile(srcf,rsfl[crs],mapd,unkid,isEncoder,addv,unkid,isEncoder)
+		crs+=1
+
+if __name__=="__main__":
+	fhead="train"
+	typ=".targ"
+	srcp="rs/"
+	rsp="mapd/"
+	nfile=40840
+	mapfl([srcp+fhead+str(i+1)+typ for i in xrange(nfile)],[rsp+fhead+str(i+1)+typ for i in xrange(nfile)],"map"+typ)
